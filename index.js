23822e8d8213f56743e13549f71687b99191b062
Francisco Puma <francis_pancho12@hotmail.com>
2021-06-19 12:34:11 -0700

    working with the index file

--- index.js
+++ index.js
@@ -1,69 +1,18 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const Employee = require("./lib/Employee");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
-const Manager = require("./lib/Manager");
+//const Employee = require("./lib/Employee");
+//const Engineer = require("./lib/Engineer");
+//const Intern = require("./lib/Intern");
+//const Manager = require("./lib/Manager");
 const teamMembers = [];
 
-const questions = [
-    {
-        type: 'input',
-        name: 'Name',
-        message: "Please enter employee's name (Required)",
-        validate: nameValue => {
-            if (nameValue) {
-                return true;
-            } else {
-                console.log('Please enter the name of the employee!');
-                return false;
-            }
-        }
-    },
-    {
-        type: 'input',
-        name: 'ID',
-        message: "Please enter employee's ID (Required)",
-        validate: employeeID => {
-            if (employeeID) {
-                return true;
-            } else {
-                console.log("Please enter employee's ID");
-                return false;
-            }
-        }
-    },
-    {
-        type: 'input',
-        name: 'Email',
-        message: "Please enter employee's email address (Required)",
-        validate: email => {
-            if (email) {
-                return true;
-            } else {
-                console.log("Please enter employee's email address");
-                return false;
-            }
-        }
-    },
-    {
-        type: 'input',
-        name: 'office number',
-        message: "Please enter employee's office number (Required)",
-        validate: officeN => {
-            if (officeN) {
-                return true;
-            } else {
-                console.log("Please enter employee's office number");
-                return false;
-            }
-        }
-    },
+const question = [
+    
     {
         type: 'list',
         name: 'Role',
         message: "Please select employee's role (Required)",
-        choices: ['Engineer', 'Manager','Intern'],
+        choices: ['Manager', 'Engineer','Intern'],
         validate: roleChoice => {
             if (roleChoice) {
                 return true;
@@ -76,15 +25,79 @@ const questions = [
 
 ]
 
-function specificQuestions();
+function specificQuestions(Role) {
+    if (Role === "Manager") {
+        managerInfo (Role);
+    }
+}
 
-addAnother ();
-generateHTML();
+;
+
+function managerInfo (Role) {
+    inquirer.prompt([
+    {
+        type: 'input',
+        name: 'Name',
+        message: "Please enter manager's name (Required)",
+        validate: nameValue => {
+            if (nameValue) {
+                return true;
+            } else {
+                console.log('Please enter the name of the manager!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'ID',
+        message: "Please enter manager's ID (Required)",
+        validate: employeeID => {
+            if (employeeID) {
+                return true;
+            } else {
+                console.log("Please enter manager's ID");
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'Email',
+        message: "Please enter manager's email address (Required)",
+        validate: email => {
+            if (email) {
+                return true;
+            } else {
+                console.log("Please enter manager's email address");
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'Office',
+        message: "Please enter manager's office number (Required)",
+        validate: officeN => {
+            if (officeN) {
+                return true;
+            } else {
+                console.log("Please enter manager's office number");
+                return false;
+            }
+        }
+    },
+    ])
+}
+
+//Engineer ();
+//Intern ();
+//generateHTML();
 
 
-//function startApp() {
-//     return inquirer.prompt(questions);
-// };
+function startApp() {
+    return inquirer.prompt(question);
+};
 
-// startApp()
-//     .then (answers => specificQuestions(answers))
+startApp()
+    .then (answer => specificQuestions(answer))
