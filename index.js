c7d178e37d6fbb2544c4827388e036b828ed4b6e
Francisco Puma <francis_pancho12@hotmail.com>
2021-06-19 11:54:07 -0700

    setting up index.js file

---
+++ index.js
@@ -0,0 +1,12 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const Employee = require("./lib/Employee");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
+
+const teamMembers = [];
+
+startApp ();
+addAnother ();
+generateHTML();
